547cc0167441726001dfdb2f48ed8cd65e0e3d2a
Elena Konovalova <elena7konovalova7@hotmail.com>
2019-10-17 19:46:44 +0300

    Create empty project

---
+++ Test/TestApp/TestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int studia = 5;
+            Console.WriteLine("Hello World = {0}", studia);
+            
+        }
+    }
+}
