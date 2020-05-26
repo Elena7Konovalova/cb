d4f18a2fe634b27fa97f9cfe84a35e8281c1d8c4
Elena Konovalova <elena7konovalova7@hotmail.com>
2020-05-26 13:59:11 +0300

    arithmetic operations

---
+++ Starter/Lesson1(2)/Lesson1(2)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Lesson1_2_
+{
+    class Program
+    {
+        static void Main ()
+        {
+            Console.WriteLine(10 - 5);
+            Console.WriteLine(10 + 5);
+            Console.WriteLine(10 * 5);
+            Console.WriteLine(10 / 5);
+            Console.WriteLine(10 % 5);
+            Console.ReadKey();
+
+            
+        }
+    }
+}
