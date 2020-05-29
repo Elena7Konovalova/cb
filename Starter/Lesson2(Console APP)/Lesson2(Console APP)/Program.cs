435b972c9bda06edc1463bafecbd8d9a6f4abec3
Elena Konovalova <elena7konovalova7@hotmail.com>
2020-05-29 22:47:45 +0300

    Variables pi and e

---
+++ Starter/Lesson2(Console APP)/Lesson2(Console APP)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lesson2_Console_APP_
+{
+    class Program
+    {
+        static void Main()
+        {
+            double pi = 3.14159265;
+            double e = 2.7182818284590452;
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
