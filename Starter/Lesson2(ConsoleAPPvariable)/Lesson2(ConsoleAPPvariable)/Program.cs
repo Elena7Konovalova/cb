ecde94c10f2399bc06585d92a8ab870b1c255082
Elena Konovalova <elena7konovalova7@hotmail.com>
2020-05-30 12:42:31 +0300

    Variable string

---
+++ Starter/Lesson2(ConsoleAPPvariable)/Lesson2(ConsoleAPPvariable)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson2_ConsoleAPPvariable_
+{
+    class Program
+    {
+        static void Main()
+        {
+            string a = "\nмоя строка 1";
+            string b = "\tмоя строка 2";
+            string c = "\aмоя строка 3";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
